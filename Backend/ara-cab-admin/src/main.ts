440bc215a3d1b8a45e8595a9ad997c63e93f08c4
raviraj16 <rraj4234@yahoo.com>
2022-09-28 19:32:16 +0530

    add location module, controller and service

--- Backend/ara-cab-admin/src/main.ts
+++ Backend/ara-cab-admin/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/admin');
+  await app.listen(parseInt(process.env.SERVER_PORT));
 }
 bootstrap();
